3a7a47fc96245c71574cd5685a7d969dda1b2d1e
Marek Topolewski <marek.topolewski@gmail.com>
2022-09-13 18:01:02 +0200

    styling: replace double with single quote

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import {
   Routes,
   Route,
   Navigate,
-} from "react-router-dom";
+} from 'react-router-dom';
 
-import Header from "./header/Header";
-import Experience from "./pages/Experience";
-import Home from "./pages/Home";
-import Services from "./pages/Services";
-import Comments from "./comments/Comments";
+import Header from './header/Header';
+import Experience from './pages/Experience';
+import Home from './pages/Home';
+import Services from './pages/Services';
+import Comments from './comments/Comments';
 import Footer from './components/Footer';
 
 const App = () => {
@@ -18,10 +18,10 @@ const App = () => {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="/experience" element={<Experience />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/*" element={<Navigate to='/home' replace />} />
+          <Route path='/home' element={<Home />} />
+          <Route path='/experience' element={<Experience />} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/*' element={<Navigate to='/home' replace />} />
         </Routes>
       </BrowserRouter>
       <Comments/>
