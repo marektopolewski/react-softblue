1ab5653b4331b0307a1bca2c9426ac8302678d2a
Marek Topolewski <marek.topolewski@gmail.com>
2022-09-13 15:11:33 +0200

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,11 @@ import Experience from "./pages/Experience";
 import Home from "./pages/Home";
 import Services from "./pages/Services";
 import Comments from "./comments/Comments";
+import Footer from './components/Footer';
 
 const App = () => {
   return (
+      <>
       <BrowserRouter>
         <Header />
         <Routes>
@@ -21,8 +23,10 @@ const App = () => {
           <Route path="/services" element={<Services />} />
           <Route path="/*" element={<Navigate to='/home' replace />} />
         </Routes>
-        <Comments/>
       </BrowserRouter>
+      <Comments/>
+      <Footer/>
+      </>
   );
 };
 
---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import classes from './Footer.module.css';
+
+const Footer = () => (
+  <div className={classes.wrapper}>
+    <span>Copyright © 2019 <b>Robi-pixel</b> All Right Reserved</span>
+  </div>
+)
+
+export default Footer;
