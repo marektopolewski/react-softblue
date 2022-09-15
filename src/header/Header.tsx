bc47a0ad8d91f65ec8a02eb925002492acac8401
Marek Topolewski <marek.topolewski@gmail.com>
2022-09-15 17:15:19 +0200

    add hello message

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -7,6 +7,17 @@ const Header: React.FC<{ onScrollToComments: () => void }> = (props) => {
     <header>
       <div className={classes['header-wrapper']}>
         <NavBar onScrollToComments={props.onScrollToComments} />
+        <div className={classes.hello}>
+          <h4>Hello!</h4>
+          <h1>I am a <span>HRS</span> Pathan</h1>
+          <h1>Creative UI/UX Designer</h1>
+          <ol>
+            <li>UI/UX Design</li>
+            <li>Product Design</li>
+            <li>Web Design</li>
+            <li>Front-end Design</li>
+          </ol>
+        </div>
       </div>
     </header>
   );
