241b3e855d2e80aa09a1246c4cb61fc23c153ff4
Marek Topolewski <marek.topolewski@gmail.com>
2022-09-15 20:40:18 +0200

    finish styling on the homepage

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -8,7 +8,7 @@ type PageHeaderProps = {
 const PageHeader: React.FC<PageHeaderProps> = (props) => (
   <div className={classes.header}>
     <h3>{props.title}</h3>
-    <h2>{props.subtitle}</h2>
+    <h1>{props.subtitle}</h1>
   </div>
 );
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,18 +8,53 @@ import useHttpRequest from '../hooks/UseHttpRequestHook';
 
 import classes from './Home.module.css';
 
+import ServiceFullyResponsive from './imgs/responsive.png'
+import ServiceDesigner from './imgs/designer.png'
+import Service247Support from './imgs/support.png'
+import ExperienceBe from './imgs/be.png'
+import ExperienceDribble from './imgs/dribble.png'
+
+const serviceIdToImage = (id: number): string => {
+  switch(id) {
+    case 1: return ServiceFullyResponsive;
+    case 2: return ServiceDesigner;
+    case 3: return Service247Support;
+  }
+  return '';
+};
+
 type ServicesItemProps = {
-  id: number
+  id: string
   name: string
   description: string
 };
+
 const ServicesItem: React.FC<ServicesItemProps> = (props) => (
   <div>
-    <p>{props.id}</p>
+    <img src={serviceIdToImage(parseInt(props.id))} alt={`service ${props.id}`} />
     <p className={classes['service-item-name']}>{props.name.toUpperCase()}</p>
     <p className={classes['service-item-desc']}>{props.description}</p>
   </div>
 );
+
+
+const experienceIdToImage = (id: number): string => {
+  switch(id) {
+    case 1: case 3: case 4: return ExperienceBe;
+    case 2: return ExperienceDribble;
+  }
+  return '';
+};
+
+const toLocaleDateString = (dateString: string) => {
+  if (!dateString || dateString === '')
+    return "";
+  const options: Intl.DateTimeFormatOptions = {
+    month: 'long',
+    year: 'numeric'
+  };
+  return (new Date(dateString)).toLocaleDateString('en-US', options);
+};
 
 type ExperienceItemProps = {
   id: string
@@ -28,13 +63,22 @@ type ExperienceItemProps = {
   from: string
   to: string
 };
+
 const ExperienceItem: React.FC<ExperienceItemProps> = (props) => (
   <div className={classes['exp-item']}>
-    <p>{props.id}</p>
-    <p className={classes['exp-item-name']}>{props.company.toUpperCase()}</p>
+    <div className={classes['exp-header']}>
+      <img src={experienceIdToImage(parseInt(props.id))} alt={`experience ${props.id}`} />
+      <div>
+        <p className={classes['exp-item-name']}>
+          {props.company}
+        </p>
+        <p className={classes['exp-item-dates']}>
+          {toLocaleDateString(props.from)} - {toLocaleDateString(props.to)}
+        </p>
+      </div>
+    </div>
+    <h3>{props.description.split(' ').splice(0, 2).join(' ')}</h3>
     <p className={classes['exp-item-desc']}>{props.description}</p>
-    <p className={classes['exp-item-desc']}>{props.from}</p>
-    <p className={classes['exp-item-desc']}>{props.to}</p>
   </div>
 );
 
