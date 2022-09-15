53608bca48e4811b8dd209e166900b2cf72cf112
Marek Topolewski <marek.topolewski@gmail.com>
2022-09-15 16:53:29 +0200

    add comments sorting

--- src/comments/CommentsBrowser.tsx
+++ src/comments/CommentsBrowser.tsx
@@ -18,6 +18,17 @@ const toLocaleDateString = (dateString: string) => {
   return (new Date(dateString)).toLocaleDateString('en-US', options);
 }
 
+type SortOrder = (-1 | 1);
+const compareStringDates = (dateString1: string, dateString2: string, order: SortOrder): number => {
+  if (dateString1 && dateString2)
+    return (new Date(dateString1).getTime() - new Date(dateString2).getTime()) * order;
+
+  // move missing dates at the end
+  if (!dateString1 || dateString1 === '')
+    return 1;
+  return -1;
+};
+
 type CommentsItemProps = {
   id: string,
   name: string,
@@ -44,8 +55,10 @@ const Comment = (item: CommentsItemProps) => (
 
 const CommentsBrowser = () => {
   const [comments,] = useHttpRequest<CommentsItemProps>('comments');
-  const onOldestClickHanlder = () => { alert('sort by oldest') };
-  const onNewestClickHanlder = () => { alert('sort by newest') };
+  const [sortOrder, setSortOrder] = useState<SortOrder>(-1);
+
+  const onOldestClickHanlder = () => { setSortOrder(1); };
+  const onNewestClickHanlder = () => { setSortOrder(-1); };
 
   const [page, setPage] = useState<number>(1);
   const changePage = (newPage: number) => {
@@ -58,15 +71,20 @@ const CommentsBrowser = () => {
       <div className={classes.header}>
         <h2>Comments</h2>
         <div className={classes.sort}>
-          <button onClick={onOldestClickHanlder}>oldest</button>
+          <button type='button' className={sortOrder === 1 ? classes['active-sort'] : ''} onClick={onOldestClickHanlder}>
+            oldest
+          </button>
           <h4>|</h4>
-          <button onClick={onNewestClickHanlder}>newest</button>
+          <button type='button' className={sortOrder === -1 ? classes['active-sort'] : ''} onClick={onNewestClickHanlder}>
+            newest
+          </button>
         </div>
       </div>
 
       {
         comments && comments.length > 0
           ? comments
+            .sort((c1, c2) => compareStringDates(c1.createdAt, c2.createdAt, sortOrder))
             .slice((page - 1) * NUM_OF_COMMENTS, page * NUM_OF_COMMENTS)
             .map(item => <Comment key={item.id} {...item} />)
           :
